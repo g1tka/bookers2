90b7504db1edbb0c96f88a4e16f8dba0c0e38492
g1tka <165782083+g1tka@users.noreply.github.com>
2024-05-17 00:32:05 +0000

    [Add]4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,19 @@
 class BooksController < ApplicationController
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+    # 成功したら、books/:idへ行きたい。×ならbooks
+  end
+  
   def index
+    @books = Book.all
   end
 
   def edit
@@ -8,12 +22,13 @@ class BooksController < ApplicationController
   def show
   end
 
-  def new
-  end
-
-  def create
-  end
-
   def destroy
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+  
 end
